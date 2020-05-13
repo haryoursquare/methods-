4056ebd16db21e5f2843c827771cd7a019acd84e
haryoursquare <mcleant91@gmail.com>
2020-05-13 15:31:08 +0100

    This is my solution

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        
+        public Decimal displayValue;
+        public Decimal currentValue;
+
+        public void Add(Decimal displayValue)
+        {
+            currentValue += displayValue;
+        }
+
+        public void Subtract(Decimal displayValue)
+        {
+            currentValue -= displayValue;
+        }
+
+        public void Multiply(Decimal displayValue)
+        {
+            currentValue *= displayValue;
+        }
+
+        public void Divide(Decimal displayValue)
+        {
+            currentValue /= displayValue;
+        }
+
+        public void SquareRoot(Decimal displayValue)
+        {
+            currentValue = (decimal)Math.Sqrt(Convert.ToDouble(displayValue));
+        }
+    }
+}
